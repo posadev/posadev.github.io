e2b7fae45ed6107297fe01e5d4ca199d427edaa8
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2025-09-21 09:34:47 -0600

    add code of conduct page and navbar

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,24 @@
 
-import React from 'react';
-import Header from '@/components/Header';
+import React, {useEffect} from 'react';
 import Hero from '@/components/Hero';
 import Gallery from '@/components/Gallery';
 import Sponsors from '@/components/Sponsors';
 import Footer from '@/components/Footer';
+import {useLocation} from "react-router-dom";
 
 const Index = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash) {
+            const element = document.querySelector(location.hash);
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    }, [location]);
   return (
     <div className="min-h-screen">
-      <Header />
       <Hero />
       <Gallery />
       <Sponsors />
