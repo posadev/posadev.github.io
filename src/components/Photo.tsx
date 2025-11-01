a7a141f3e66ab032edc3c281ef51747576b29877
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2025-11-01 09:09:26 -0600

    remove heavy imgs

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -27,11 +27,10 @@ const Photo: React.FC<PhotoProps> = ({image, setSelectedImage, index}) => {
                   alt={image.alt}
                   decoding="async"
                   loading="lazy"
-                  fetchPriority="low"
-                  className="w-full object-cover transition-transform duration-500 group-hover:scale-110 hover-scale"
+                  className="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-110 hover-scale"
               />
               {/* Overlay */}
-              <div className="absolute inset-0 bg-black/60 group-hover:bg-black/60 flex justify-between align-middle via-transparent to-transparent opacity-100 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="absolute inset-0 bg-black/20  lg:group-hover:bg-black/60 flex justify-between align-middle via-transparent to-transparent opacity-100 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                   <figcaption
                       className="text-white font-semibold text-lg mb-2 flex flex-col justify-end p-4">
                       <span>{image.title}</span>
--- src/components/Speaker.tsx
+++ src/components/Speaker.tsx
@@ -18,10 +18,10 @@ const Speaker: React.FC<SpeakerProps> = ({speaker}) => {
     return (
         <article className="h-full w-full mx-1 flex flex-col justify-center items-center">
                 <img src={speaker.profilePicture} alt={speaker.fullName} className="h-60 w-60 rounded-3xl" loading="lazy"/>
-                <Card className="bg-white/10 border-0 shadow-none text-white flex flex-col justify-start h-52 p-6 min-w-[100%] max-w-[400px]">
+                <Card className="bg-white/10 border-0 shadow-none text-white flex flex-col justify-start gap-4 h-52 p-6 min-w-[100%] max-w-[400px]">
                     <p className="text-2xl font-bold">{`${speaker.firstName.split(" ")[0]} ${speaker.lastName.split(" ")[0]}`}</p>
-                    <p className="py-2 text-xl h-full line-clamp-2 overflow-hidden my-4 rounded">{speaker.tagLine}</p>
-                    <div className="flex justify-self-end items-end justify-start gap-6 md:gap-2 lg:gap-4 text-alternative-100">
+                    <p className="my-2 text-xl break-words line-clamp-2 overflow-hidden flex-shrink-0 rounded">{speaker.tagLine}</p>
+                    <div className="flex h-full justify-self-end items-end justify-start gap-6 md:gap-2 lg:gap-4 text-alternative-100">
                         {speaker.links.map((link, index) => (
                             <SocialMedia key={`link-${link.url}`} link={link} />
                         ))}
