fdf764e77dd8cc3157253420d6f3626060026b25
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2025-11-04 13:28:15 -0600

    add code of conduct speakers and refactor ui

---
+++ src/components/code-of-conduct/Report.tsx
@@ -0,0 +1,27 @@
+import {Card} from "@/components/ui/card.tsx";
+
+const Report = () => {
+  return (
+      <Card className="p-10 flex flex-col gap-6">
+          <h2 className="text-3xl text-alternative-700 font-semibold">Reporte</h2>
+          <p>
+              Si alguien te hace sentir inseguro o no bienvenido a ti o a otra persona, por favor repórtalo lo antes posible. Si ves o escuchas que alguien hace algo que pueda violar este Código de Conducta, repórtalo. Puedes presentar un informe personal mediante:
+          </p>
+          <section className="flex flex-col md:flex-row gap-4">
+              <article className="w-full md:w-1/3">
+                  <strong className="text-primary-600">Organizador</strong>
+                  <p>Acércate a cualquier miembro del equipo organizador, quienes estarán identificados con una camiseta con la palabra "Organizador".</p>
+              </article>
+              <article className="w-full md:w-1/3">
+                  <strong className="text-primary-600">Correo Electrónico</strong>
+                  <p>Envía un correo a <a className="text-primary-600 hover:underline hover:underline-offset-1" href="mailto:gdldevcomss@gmail.com">gdldevcomms@gmail.com</a> con el asunto "Reporte"</p>
+              </article>
+              <article className="w-full md:w-1/3">
+                  <strong className="text-primary-600">Redes DMs</strong>
+                  <p>Envía un mensaje directo a cualquiera de nuestras redes sociales oficiales.</p>
+              </article>
+          </section>
+      </Card>
+  )
+}
+export default Report;
---
+++ src/pages/CodeOfConductSpeakers.tsx
@@ -0,0 +1,61 @@
+import {Link} from "react-router-dom";
+import Gradient from "@/components/Gradient.tsx";
+import {Card} from "@/components/ui/card.tsx";
+import Report from "@/components/code-of-conduct/Report.tsx";
+
+const CodeOfConductSpeakers = () => {
+  return (
+    <Gradient
+        className="flex flex-col gap-8 px-20 py-10"
+        aria-labelledby="code-of-conduct-heading"
+    >
+        <aside className="flex flex-col gap-2">
+            <h1 className="text-4xl gradient-text">Código de conducta de Speakers</h1>
+            <p className="text-lg" itemProp="description">
+                Todo speaker debera cumplir con el <Link role="link" to={"/code-of-conduct"} className="text-primary-600 hover:underline hover:underline-offset-1">codigo de conducta general</Link> al igual que los siguientes puntos puestos en este documento:
+            </p>
+        </aside>
+      <Card className="p-10 flex flex-col gap-6">
+          <article className="flex flex-col gap-2">
+              <h2 className="text-3xl text-alternative-700 font-semibold">Material ofensivo</h2>
+              <p>
+                  Al igual que el comportamiento inaceptable no es algo que vamos a tolerar en posadev tampoco el material físico o oral que falle con los puntos siguientes:
+              </p>
+              <ul className="list-disc list-inside">
+                  <li>
+                      Cualquier acción o comunicación  (intimidatorias, vejatorias, discriminatorias, violentas, abusivas, denigratorias, despectivas, sexistas o cualquiera que denigre a alguna minoria) será un comportamiento inaceptable asi como los chistes y referencias
+                  </li>
+                  <li>
+                      Tambien se incluye los puntos estipulados en comportamiento inaceptable que no deben ser agregados en el material utilizado para su presentacion
+                  </li>
+              </ul>
+          </article>
+          <article className="flex flex-col gap-2">
+              <h2 className="text-3xl font-semibold text-alternative-700">Consecuencias</h2>
+              <p>
+                  Cualquier speaker que infrinja este código podrá ser sancionado de acuerdo a la deliberación del equipo organizador:
+              </p>
+              <ul className="list-disc list-inside">
+                  <strong className="text-alternative-700">Antes de la conferencia:</strong>
+                  <li>Dependiendo de la gravedad se puede cancelar su participacion como speaker (al igual que nos reservamos el derecho de admision)</li>
+              </ul>
+              <ul className="list-disc list-inside">
+                  <strong className="text-alternative-700">Durante la conferencia:</strong>
+                  <li>Cancelacion de la participacion y solicitar que se retire</li>
+                  <li>Llamado de atencion y un aviso de que al continuar su conducta se le retirara del evento</li>
+              </ul>
+              <ul className="list-disc list-inside">
+                  <strong className="text-alternative-700">Despues de la conferencia:</strong>
+                  <li>En el caso de eventos sociales organizados por posadev nos reservamos pedir que se retire y al no respetarlo llevarlo con las autoridades correspondientes</li>
+                  <li>Veto de eventos relacionados de GDL Devcomms (Esto incluye ediciones futuras de Posadev)</li>
+              </ul>
+          </article>
+      </Card>
+        <Report/>
+        <p>
+            Al asistir a este evento, aceptas cumplir este Código de Conducta. Agradecemos tu participación para hacer de este un espacio inclusivo, seguro y respetuoso para todxs.
+        </p>
+    </Gradient>
+  )
+}
+export default CodeOfConductSpeakers;
