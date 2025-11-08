5b79b6efbc25b61b077edc436fb963645ce1d433
Sierisimo Sier <sierisimo@gmail.com>
2025-11-08 17:01:53 -0600

    Update eslint, tseslint and config for tscompiler

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, tseslint.configs.stylisticTypeChecked, tseslint.configs.strictTypeChecked],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,6 +25,13 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
     },
   }
 );
