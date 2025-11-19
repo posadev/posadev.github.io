85d91b51ed56bce158ca1cbd714e38b825312951
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2025-11-19 15:06:27 -0600

    improve ui

--- src/components/agenda/AgendaContent.tsx
+++ src/components/agenda/AgendaContent.tsx
@@ -4,6 +4,7 @@ import {useEffect} from "react";
 import {getAgenda} from "@/https/fetch.ts";
 import {useAppContext} from "@/context/AppContext.tsx";
 import AgendaRow from "@/components/agenda/AgendaRow.tsx";
+import {ScrollArea} from "@/components/ui/scroll-area.tsx";
 
 const AgendaContent = () => {
     const {setAgenda, agenda, savedSessions} = useAppContext()
@@ -19,23 +20,23 @@ const AgendaContent = () => {
 
     if (!displayAll && savedSessions.size === 0) {
         return (<Card className="w-full pb-8 md:px-6 relative shadow-gray-500">
-            <nav className="flex gap-4 pt-8 pb-2 sticky bg-white w-full">
+            <nav className="flex gap-4 pt-8 pb-2 sticky bg-white w-full h-20">
                 <Badge variant={`${displayAll ? "default" : "ghost"}`} onClick={() => setDisplayAll(true)} role="button">Display All</Badge>
                 <Badge variant={`${displayAll ? "ghost" : "default"}`} onClick={() => setDisplayAll(false)} role="button">Display Saved</Badge>
             </nav>
-            <h2 className="text-center text-alternative-700 text-2xl font-bold">Por favor guarda alguna sesion para verla aqui</h2>
+            <h2 className="text-center text-alternative-700 text-2xl font-bold mt-6 sm:mt-2">Por favor guarda alguna sesion para verla aqui</h2>
         </Card>)
     }
 
   return (
       <Card className="w-full pb-8 md:px-6 relative shadow-gray-500">
-          <nav className="flex gap-4 pt-8 pb-2 sticky bg-white w-full">
+          <nav className="flex gap-4 pt-8 pb-2 sticky top-[80px] z-10 bg-white w-full h-20">
               <Badge variant={`${displayAll ? "default" : "ghost"}`} onClick={() => setDisplayAll(true)} role="button">Display All</Badge>
               <Badge variant={`${displayAll ? "ghost" : "default"}`} onClick={() => setDisplayAll(false)} role="button">Display Saved</Badge>
           </nav>
-          {agenda.map(({slotStart, rooms}) => (
-              <AgendaRow key={slotStart} slotStart={slotStart} rooms={rooms}/>
-          ))}
+              {agenda.map(({slotStart, rooms}) => (
+                  <AgendaRow key={slotStart} slotStart={slotStart} rooms={rooms}/>
+              ))}
       </Card>
   )
 }
