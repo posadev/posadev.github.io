6b2aa00296b3d3fb82eddcac63cfbfe267581f1e
Yaidi García <56900133+Yaidi@users.noreply.github.com>
2025-11-19 17:17:17 -0600

    fix datimes

--- src/components/agenda/AgendaItem.tsx
+++ src/components/agenda/AgendaItem.tsx
@@ -1,5 +1,18 @@
-const AgendaItem = ({ icon: Icon, title, time }) => {
-    const [start, end] = time.split(" - ");
+import React from "react";
+
+interface AgendaItemProps {
+    icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+    title: string;
+    time: Array<string>;
+}
+
+const AgendaItem: React.FC<AgendaItemProps> = ({ icon: Icon, title, time }) => {
+
+    const splitTime = (timeString: string) => {
+        const [start, end] = timeString.split(" - ");
+        return { start, end };
+    }
+    const isSingleTime = time.length === 1;
 
     return (
       <aside className="flex flex-col items-center text-center gap-2 w-[180px] hover:scale-105 transition-transform">
@@ -7,11 +20,15 @@ const AgendaItem = ({ icon: Icon, title, time }) => {
         {Icon && <Icon className="h-6 w-6 text-primary-500" />}
       </span>
             <p className="font-medium text-primary-800">{title}</p>
-          <time className="flex flex-col items-center text-primary-600 text-sm leading-tight">
-              <span>{start}</span>
-              {end && <span className="text-xs">-</span>}
-              <span>{end}</span>
-          </time>
+          {
+              time.map((timeString, index) => (
+                  <time key={`time-${index}`} className={`flex ${isSingleTime ? "flex-col" : "flex-row flex-wrap gap-2"} items-center text-primary-600 text-sm leading-tight`}>
+                      <span>{splitTime(timeString).start}</span>
+                      {splitTime(timeString).end && <span className="text-xs">-</span>}
+                      <span>{splitTime(timeString).end}</span>
+                  </time>
+              ))
+          }
         </aside>
     )
 }
