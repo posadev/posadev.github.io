8dcc375a4b10f89b7123a7136ac6fbe0d287685b
Christian Gomez <omar.christian@gmail.com>
2026-09-22 17:41:01 -0600

    Adding more communities

--- src/data/communites.ts
+++ src/data/communites.ts
@@ -4,6 +4,8 @@ import technologyEventsLogo from "@/img/communities/2026/tech-events.png";
 import mdc from "@/img/communities/2025/mdc.png";
 import playasOnTechLogo from "@/img/communities/2025/playas-logo.webp";
 import GDGLogo from "@/img/communities/2025/GDG-Guadalajara.png";
+import WOSECLogo from "@/img/communities/2025/WoSEC.png";
+import ESGLogo from "@/img/communities/2026/ESG.png"
 import ajolotesAWSLogo from "@/img/communities/2025/ajolotes-aws.png";
 import calzadacode from "@/img/communities/2025/calzadacode.png";
 import linuxeroszapopan from "@/img/communities/2025/linuxeroszapopan.png";
@@ -14,31 +16,16 @@ import kcdLogo from "@/img/communities/2025/kcd.jpeg"
 
 export const communitiesAllies: Array<ICommunity> = [
     {
-        name: "Technology Events",
+        name: "Embedded Systems Geeks",
         description: "",
-        link: "https://www.facebook.com/share/19r2SzioLV/",
-        image: technologyEventsLogo,
-        sticker: false,
+        link: "https://www.linkedin.com/in/frank-zeyda/",
+        image: ESGLogo,
+        sticker: true,
         socials: [
             {
-            title: "Instagram",
-            url: "https://www.instagram.com/technology_events_community?igsh=MXgydXFnYWRpcnRseg==",
-            linkType: LinkType.Instagram
-            },
-            {
-                title: "Facebook",
-                url: "https://www.facebook.com/share/19r2SzioLV/",
-                linkType: LinkType.Facebook
-            },
-            {
                 title: "LinkedIn",
-                url: "https://www.linkedin.com/company/technology-events-community/",
+                url: "https://www.linkedin.com/in/frank-zeyda//",
                 linkType: LinkType.LinkedIn
-            },
-            {
-                title: "Tiktok",
-                url: "https://www.tiktok.com/@technology.events?_t=ZS-8zTq13UhIl0&_r=1",
-                linkType: LinkType.TikTok
             }
         ]
     },
@@ -106,23 +93,25 @@ export const communitiesAllies: Array<ICommunity> = [
             linkType: LinkType.LinkedIn
         }
         ]
-    }/*,
+    },
     {
-        name: "AWS User Group Ajolotes en la Nube ",
+        name: "WOSEC MEXICO",
         description: "",
-        link: "",
-        image: ajolotesAWSLogo,
-        socials: [
-            {
-                title: "Instagram",
-                url: "https://www.instagram.com/aws.ajolotes?igsh=MTk1ajFiZHdqand0aA%3D%3D&utm_source=qr",
-                linkType: LinkType.Instagram
-            },
-            {
-                title: "LinkedIn",
-                url: "https://www.linkedin.com/company/aws-ug-ajolotes-en-la-nube/",
-                linkType: LinkType.LinkedIn
-            }
+        link: "https://wosecmexico.org/",
+        image: WOSECLogo,
+        socials: [{
+            title: "Facebook",
+            url: "https://www.facebook.com/61576410909269",
+            linkType: LinkType.Facebook
+        },{
+            title: "Instagram",
+            url: "https://www.instagram.com/wosec_mx/",
+            linkType: LinkType.Instagram
+        },{
+            title: "LinkedIn",
+            url: "https://www.linkedin.com/company/wosecmexico/?viewAsMember=true",
+            linkType: LinkType.LinkedIn
+        }
         ]
     },
     {
@@ -171,6 +160,30 @@ export const communitiesAllies: Array<ICommunity> = [
             }
         ]
     },
+    {
+        name: "WebDevTalks",
+        description: "",
+        link: "https://webdevtalks.mx",
+        image: webdevtalksLogo,
+        sticker: false,
+        socials: [
+            {
+                title: "Instagram",
+                url: "https://www.instagram.com/webdevtalksmx",
+                linkType: LinkType.Instagram
+            },
+            {
+                title: "Facebook",
+                url: "https://www.facebook.com/ColimaWebDevTalks",
+                linkType: LinkType.Facebook
+            },
+            {
+                title: "LinkedIn",
+                url: "https://www.linkedin.com/company/web-dev-talks",
+                linkType: LinkType.LinkedIn
+            }
+        ]
+    }/*,
     {
         name: "TechQuiero",
         description: "",
